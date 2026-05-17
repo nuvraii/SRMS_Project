aa7d82fb5ac6c5e34d10318aa4911736314914c0
Fajer-ship-fa <fajer.a1425@gmail.com>
2026-05-17 13:03:37 +0300

    Generates and manages reports for student data
    This class is responsible for generating different types of student reports. It provides methods to display the top student based on GPA, filter students by department, count total students, and calculate the average GPA of all students in the system.

---
+++ SaveThread.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class SaveThread extends Thread {
+
+    private FileManager fileManager;
+    private ArrayList<StudentData> students;
+
+    // Constructor
+    public SaveThread(FileManager fileManager, ArrayList<StudentData> students) {
+
+        this.fileManager = fileManager;
+        this.students = students;
+    }
+
+    // Thread task
+    @Override
+    public void run() {
+
+        fileManager.save_To_File(students);
+
+        System.out.println("Data saved using thread successfully.");
+    }
+}s
---
+++ StudentReportGenerator.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+public class StudentReportGenerator {
+
+    // Show the student with highest GPA
+    public void showTopStudents(ArrayList<StudentData> students) {
+
+        if (students.isEmpty()) {
+            System.out.println("No students found.");
+            return;
+        }
+
+        StudentData topStudent = students.get(0);
+
+        for (StudentData s : students) {
+            if (s.getGpa() > topStudent.getGpa()) {
+                topStudent = s;
+            }
+        }
+
+        System.out.println("---- Top Student ----");
+        System.out.println(topStudent);
+    }
+
+    // Show students from specific department
+    public void showStudentsByDepartment(ArrayList<StudentData> students, String department) {
+
+        boolean found = false;
+
+        for (StudentData s : students) {
+            if (s.getDepartment().equalsIgnoreCase(department)) {
+                System.out.println(s);
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No students found in this department.");
+        }
+    }
+
+    // Count all students
+    public void countStudents(ArrayList<StudentData> students) {
+        System.out.println("Total Students: " + students.size());
+    }
+
+    // Calculate average GPA
+    public void showAverageGPA(ArrayList<StudentData> students) {
+
+        if (students.isEmpty()) {
+            System.out.println("No students available.");
+            return;
+        }
+
+        double total = 0;
+
+        for (StudentData s : students) {
+            total += s.getGpa();
+        }
+
+        double average = total / students.size();
+
+        System.out.println("Average GPA: " + average);
+    }
+}
